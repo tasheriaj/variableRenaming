85416a71314d470c2cd8e361115e5965f22f0a13
tasheriaj <tasheriaj@gmail.com>
2022-09-15 18:41:31 -0500

    warmUp

---
+++ javaVariables/src/javaVariables/Main.java
@@ -0,0 +1,17 @@
+package javaVariables;
+
+public class Main {
+public static void main(String[] args) {
+	boolean pickOne  = true;
+	byte hue1 = 31;
+	short purple = -432;
+	int hue2 = 200;
+	long purp2 = 404323333L;
+	double digIts = 12.5;
+	float dBle = 25.2f;
+	char aBet = '\uee31';
+
+	
+	System.out.println("Team Jones");
+}
+}
